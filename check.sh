be3ce3cc5e2de55117f1eca6c793ef80aca1a3a4
Greg Lowe <greg@vis.net.nz>
2013-04-05 16:16:45 +1300

    Mark escape tags as unimplemented

--- check.sh
+++ check.sh
@@ -3,7 +3,6 @@
 # Abort if non-zero code returned.
 set -e
 
-dart_analyzer --type-checks-for-inferred-types lib/mustache.dart
 dart_analyzer --type-checks-for-inferred-types test/mustache_test.dart
 
 dart --checked test/mustache_test.dart
