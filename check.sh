921728a048419b463e9f7b2ee2f300cf977afd75
Greg Lowe <greg@vis.net.nz>
2013-04-05 16:05:27 +1300

    Add strict/lenient mode

--- check.sh
+++ check.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
+
+# Abort if non-zero code returned.
+set -e
+
 dart_analyzer --type-checks-for-inferred-types lib/mustache.dart
+dart_analyzer --type-checks-for-inferred-types test/mustache_test.dart
 
 dart --checked test/mustache_test.dart
 
