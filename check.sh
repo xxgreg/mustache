024fa564495d449f3a2ed8437dfc690380e0a683
Greg Lowe <greg@vis.net.nz>
2013-06-14 11:48:49 +1200

    Update to new analyzer

--- check.sh
+++ check.sh
@@ -3,7 +3,7 @@
 # Abort if non-zero code returned.
 set -e
 
-dart_analyzer --type-checks-for-inferred-types test/mustache_test.dart
+dartanalyzer test/mustache_test.dart
 
 dart --checked test/mustache_test.dart
 
