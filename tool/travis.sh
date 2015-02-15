2fe183b41c51a4a0c7df666962eee38659afc0e6
Greg Lowe <greg@vis.net.nz>
2015-02-15 16:44:27 +1300

    Fix travis/coveralls integration

--- tool/travis.sh
+++ tool/travis.sh
@@ -36,6 +36,7 @@ git clone https://github.com/mustache/spec.git test/spec
 dart --checked test/all.dart
 
 # Gather and send coverage data.
-pub global activate dart_coveralls
-pub global run dart_coveralls report --token $REPO_TOKEN --retry 2 --exclude-test-files test/all.dart
-
+if [ "$REPO_TOKEN" ]; then
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report --token $REPO_TOKEN --retry 2 --exclude-test-files test/all.dart
+fi
